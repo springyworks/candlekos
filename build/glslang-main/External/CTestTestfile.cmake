dc68e23eb7dbc94375ec6db8f2bf66d7be2306ae
springyworks <kostoel@gmail.com>
2025-08-16 18:23:27 +0200

    CI: tighten secret scanning, ignore generated artifacts\n\n- Add .trufflehogignore to exclude build/, caches, third_party, binaries\n- Wire trufflehog action to use exclude paths\n- Ignore build/ and .cache/ in VCS; untrack committed artifacts\n\nThis should eliminate false positives like 'unverified GitHub result' in build/glslang-main/CHANGES.md and README links.

--- build/glslang-main/External/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/rustuser/projects/rust/from_github/candle/third_party/VkFFT/glslang-main/External
-# Build directory: /home/rustuser/projects/rust/from_github/candle/build/glslang-main/External
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/glslang-main/SPIRV/SPVRemapperTargets.cmake
+++
@@ -1,9 +0,0 @@
-
-            message(WARNING "Using `SPVRemapperTargets.cmake` is deprecated: use `find_package(glslang)` to find glslang CMake targets.")
-
-            if (NOT TARGET glslang::SPVRemapper)
-                include("/usr/local/lib/cmake/glslang/glslang-targets.cmake")
-            endif()
-
-            add_library(SPVRemapper ALIAS glslang::SPVRemapper)
-        

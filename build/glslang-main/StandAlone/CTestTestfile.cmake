8c6ba06d8cbdb401136db248277ed67096bb3231
springyworks <kostoel@gmail.com>
2025-08-16 12:44:59 +0200

    chore: untrack build/ and .cache from VCS; keep build/README.md tracked

--- build/glslang-main/StandAlone/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/rustuser/projects/rust/from_github/candle/third_party/VkFFT/glslang-main/StandAlone
-# Build directory: /home/rustuser/projects/rust/from_github/candle/build/glslang-main/StandAlone
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/glslang-main/glslang/OSDependent/Unix/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/rustuser/projects/rust/from_github/candle/third_party/VkFFT/glslang-main/glslang/OSDependent/Unix
-# Build directory: /home/rustuser/projects/rust/from_github/candle/build/glslang-main/glslang/OSDependent/Unix
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
